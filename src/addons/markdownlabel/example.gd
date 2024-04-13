b17c4191c7022acc52e615426963bb0b4c3d10a3
Brenan <lundquist.brenan@gmail.com>
2024-04-13 13:26:01 -0700

    modified md addon for llm style code blocks

---
+++ src/addons/markdownlabel/example.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+func _ready() -> void:
+	#$MarkdownLabel.display_file("res://addons/markdownlabel/README.md")
+	$MarkdownLabel.display_file("res://addons/markdownlabel/code_test.md")
