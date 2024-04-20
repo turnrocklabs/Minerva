2a51c8dbb50690bf2403ee534483b76e9ba94228
DzIGIBAUER <milosh2255@gmail.com>
2024-04-20 21:54:16 +0200

    Added drag icon

--- src/Scripts/UI/Controls/Note.gd
+++ src/Scripts/UI/Controls/Note.gd
@@ -1,21 +1,30 @@
 class_name Note
 extends PanelContainer
 
-@onready var label_node: LineEdit = $VBoxContainer/Title
-@onready var description_node: TextEdit = $VBoxContainer/Description
+@onready var label_node: LineEdit = $h/v/HBoxContainer/Title
+@onready var description_node: TextEdit = $h/v/Description
+@onready var checkbutton_node: CheckButton = $h/v/HBoxContainer/CheckButton
 
 @export var title: String
 @export var description: String
 
 
-var memory_item: MemoryItem:
+var memory_item: MemoryItem = MemoryItem.new("TEST NOTE BREE"):
 	set(value):
 		if not value: return value
-		
 		label_node.text = value.Title
 		description_node.text = value.Content
+		checkbutton_node.toggle_mode = value.Enabled
 
-		memory_item = value
+		return value
+
+
+func _ready():
+	memory_item.Title = title
+	memory_item.Content = description
+
+	label_node.text = title
+	description_node.text = description
 
 
 func _notification(notification_type):
@@ -53,16 +62,17 @@ func _can_drop_data(_at_position: Vector2, data):
 	if not data is Note: return false
 	
 	for node in get_parent().get_children():
-
-		if node.get_rect().has_point(_at_position) and node is Note:
+		if node is Note and node.get_rect().has_point(_at_position):
 			_replace_nodes(data, self)
 
 	return true
 
-func _drop_data(_at_position: Vector2, data):
+func _drop_data(at_position: Vector2, data):
 	data = data as Note
-
-	print("%s -> %s" % [data.get_index(), get_index()])
 
 	_replace_nodes(data, self)
 	
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	memory_item.Enabled = toggled_on
