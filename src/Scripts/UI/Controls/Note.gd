cee59c9022292747f336bc0c8eaa59183a176520
DzIGIBAUER <milosh2255@gmail.com>
2024-04-20 22:04:17 +0200

    Added comments

--- src/Scripts/UI/Controls/Note.gd
+++ src/Scripts/UI/Controls/Note.gd
@@ -8,8 +8,8 @@ extends PanelContainer
 @export var title: String
 @export var description: String
 
-
-var memory_item: MemoryItem = MemoryItem.new("TEST NOTE BREE"):
+# this will react each time memory item is changed
+var memory_item: MemoryItem:
 	set(value):
 		if not value: return value
 		label_node.text = value.Title
@@ -18,15 +18,7 @@ var memory_item: MemoryItem = MemoryItem.new("TEST NOTE BREE"):
 
 		return value
 
-
-func _ready():
-	memory_item.Title = title
-	memory_item.Content = description
-
-	label_node.text = title
-	description_node.text = description
-
-
+# when the drop is finished, show the dragged item again
 func _notification(notification_type):
 	match notification_type:
 		NOTIFICATION_DRAG_END:
@@ -36,13 +28,11 @@ func _notification(notification_type):
 func _replace_nodes(node1: Node, node2: Node) -> void:
 	var dragged_node_index = node1.get_index()
 
-	# var tween = get_tree().create_tween()
-	# tween.tween_property(node2, "position:y", node1.position.y, 0.1)
-
 	get_parent().move_child(node1, get_index())
 	get_parent().move_child(node2, dragged_node_index)
 
-
+# will create a preview of the node that we are draggind and hide the original node
+# the preview is just a duplicate of the original
 func _get_drag_data(at_position: Vector2) -> Note:
 
 	var preview = Container.new()
@@ -54,10 +44,13 @@ func _get_drag_data(at_position: Vector2) -> Note:
 
 	set_drag_preview(preview)
 
+	# hide the original, and show it again on drag end
 	modulate.a = 0
 
 	return self
 
+# this will check if we dragged the node above another Note node
+# if yes, swich places with the original(hidden) node
 func _can_drop_data(_at_position: Vector2, data):
 	if not data is Note: return false
 	
@@ -67,12 +60,13 @@ func _can_drop_data(_at_position: Vector2, data):
 
 	return true
 
+# if we dropped the node above another replace them with each other
 func _drop_data(at_position: Vector2, data):
 	data = data as Note
 
 	_replace_nodes(data, self)
 	
 
-
+# if the check button is clicked update the MemoryItem state to reflect that
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	memory_item.Enabled = toggled_on
