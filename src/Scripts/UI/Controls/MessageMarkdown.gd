ee9686a578e2a103c299b702a7b714faf3b87bc5
DzIGIBAUER <milosh2255@gmail.com>
2024-05-05 03:36:27 +0200

    Added code extract function

--- src/Scripts/UI/Controls/MessageMarkdown.gd
+++ src/Scripts/UI/Controls/MessageMarkdown.gd
@@ -3,7 +3,7 @@ extends HBoxContainer
 
 @export var left_control: Control
 @export var right_control: Control
-@export var label: RichTextLabel
+@export var label: MarkdownLabel
 
 @export var user_message_color: Color
 @export var bot_message_color: Color
@@ -13,7 +13,6 @@ static func bot_message(message: BotResponse) -> MessageMarkdown:
 	msg.right_control.visible = true
 	msg.label.markdown_text = message.FullText
 	msg.label.set("theme_override_colors/default_color", Color.BLACK)
-	
 	
 	var style: StyleBox = msg.get_node("PanelContainer").get("theme_override_styles/panel")
 	style.bg_color = msg.bot_message_color
@@ -31,3 +30,63 @@ static func user_message(message: BotResponse) -> MessageMarkdown:
 	style.bg_color = msg.user_message_color
 
 	return msg
+
+
+func extract_code_block(source_text = "") -> String:
+
+	var code_block = ""
+	
+	var lines = source_text.split("\n")
+	var _indent_level = -1
+	var iline := 0
+	var within_backtick_block := false
+	var within_tilde_block := false
+	var within_code_block := false
+	var current_code_block_char_count: int
+	var _current_paragraph = 0
+	var _line_break = true
+
+	for line in lines:
+		line = line.trim_suffix("\r")
+		within_code_block = within_tilde_block or within_backtick_block
+		if iline > 0 and _line_break:
+			# _converted_text += "\n"
+			_current_paragraph += 1
+			_line_break = true
+		iline+=1
+		
+		if not within_tilde_block and label._denotes_fenced_code_block(line,"`"):
+			if within_backtick_block:
+				if line.strip_edges().length() >= current_code_block_char_count:
+					code_block = code_block.trim_suffix("\n")
+					_current_paragraph -= 1
+					# code_block += "[/code]"
+					within_backtick_block = false
+					continue
+			else:
+				# code_block += "[code]"
+				within_backtick_block = true
+				#current_code_block_char_count = line.strip_edges().length()
+				current_code_block_char_count = label._get_codeblock_char_count(line, "`")
+				continue
+		elif not within_backtick_block and label._denotes_fenced_code_block(line,"~"):
+			if within_tilde_block:
+				if line.strip_edges().length() >= current_code_block_char_count:
+					code_block = code_block.trim_suffix("\n")
+					_current_paragraph -= 1
+					# code_block += "[/code]"
+					within_tilde_block = false
+					continue
+			else:
+				# code_block += "[code]"
+				within_tilde_block = true
+				current_code_block_char_count = line.strip_edges().length()
+				continue
+		if within_code_block: #ignore any formatting inside code block
+			code_block += label._escape_bbcode(line)
+			continue
+	
+	return code_block
+
+func _on_markdown_label_gui_input(event:InputEvent):
+	print(event)
