ed479c6a6baa5169680323ed7474c3e300b2c0c5
DzIGIBAUER <milosh2255@gmail.com>
2024-05-08 22:50:33 +0200

    MessageMarkdown now displayed code blocks in separate container with copy button

--- src/Scripts/UI/Controls/MessageMarkdown.gd
+++ src/Scripts/UI/Controls/MessageMarkdown.gd
@@ -26,95 +26,76 @@ static func user_message(message: BotResponse) -> MessageMarkdown:
 	msg.label.markdown_text = message.FullText
 	msg.label.set("theme_override_colors/default_color", Color.WHITE)
 
-	var style: StyleBox = msg.get_node("PanelContainer").get("theme_override_styles/panel")
+	var style: StyleBoxFlat = msg.get_node("PanelContainer").get("theme_override_styles/panel")
 	style.bg_color = msg.user_message_color
 
 	return msg
 
 
+class TextSegment:
+	var syntax: String
+	var content: String
+
+	func _init(content_: String, syntax_: String = ""):
+		content = content_
+		syntax = syntax_
+	
+	func _to_string():
+		if syntax:
+			return "%s: %s" % [syntax, content]
+		else:
+			return content
+
+
 func _ready():
 	var regex = RegEx.new()
-	var err = regex.compile(r"(\[code\])((.|\n)*)(\[\/code\])")
+	regex.compile(r"(\[code\])((.|\n)*?)(\[\/code\])")
 
-	var matches = regex.search_all(label.text)
-	for m in matches:
+	var text = label.text
+
+	var mathces = regex.search_all(label.text)
+
+	var text_segments: Array[TextSegment] = []
+
+	for m in mathces:
 		var code_text = m.get_string()
+		var one_line = code_text.count("\n") == 0
 
-		var first_part_len = label.text.find(code_text)
+		if one_line: continue
+
+		var first_part_len = text.find(code_text)
 		
 		var second_part_start = first_part_len + code_text.length()
-		var second_part_len = label.text.length()
+		var second_part_len = text.length()
 
-		var first_label = label.duplicate()
-		first_label.text = label.text.substr(0, first_part_len).strip_edges()
-		$PanelContainer/v.add_child(first_label)
-		$PanelContainer/v.move_child(first_label, label.get_index())
+		var ts1 = TextSegment.new(text.substr(0, first_part_len).strip_edges())
 
-		var second_label = label.duplicate()
-		second_label.text = label.text.substr(second_part_start, second_part_len).strip_edges()
-		$PanelContainer/v.add_child(second_label)
-		$PanelContainer/v.move_child(second_label, label.get_index()+1)
+		var ts3 = TextSegment.new(text.substr(second_part_start, second_part_len).strip_edges())
 
-		label.text = label.text.substr(first_part_len, code_text.length()).strip_edges()
-		label.get_node("CopyButton").visible = true
-		label.get_node("CopyButton").pressed.connect(func(): DisplayServer.clipboard_set(code_text))
+		# first line of the markdown text eg. ```python
+		var syntax = label.markdown_text.substr(first_part_len, code_text.length()).strip_edges().split("\n")[0]
+		syntax = syntax.replace("`", "")
 
+		var ts2 = TextSegment.new(code_text, syntax)
 
-func extract_code_block(source_text = "") -> String:
-
-	var code_block = ""
+		text_segments.append(ts1)
+		text_segments.append(ts2)
+		text_segments.append(ts3)
 	
-	var lines = source_text.split("\n")
-	var _indent_level = -1
-	var iline := 0
-	var within_backtick_block := false
-	var within_tilde_block := false
-	var within_code_block := false
-	var current_code_block_char_count: int
-	var _current_paragraph = 0
-	var _line_break = true
+	if not mathces: return
 
-	for line in lines:
-		line = line.trim_suffix("\r")
-		within_code_block = within_tilde_block or within_backtick_block
-		if iline > 0 and _line_break:
-			# _converted_text += "\n"
-			_current_paragraph += 1
-			_line_break = true
-		iline+=1
+	# clear all children
+	for ch in label.get_parent().get_children(): label.get_parent().remove_child(ch)
+	for ts in text_segments:
+		var node: Node
+
+		if ts.syntax:
+			node = CodeMarkdownLabel.create(ts.content, ts.syntax)
+		else:
+			node = RichTextLabel.new()
+			node.fit_content = true
+			node.bbcode_enabled = true
+			node.text = ts.content
 		
-		if not within_tilde_block and label._denotes_fenced_code_block(line,"`"):
-			if within_backtick_block:
-				if line.strip_edges().length() >= current_code_block_char_count:
-					code_block = code_block.trim_suffix("\n")
-					_current_paragraph -= 1
-					# code_block += "[/code]"
-					within_backtick_block = false
-					continue
-			else:
-				# code_block += "[code]"
-				within_backtick_block = true
-				#current_code_block_char_count = line.strip_edges().length()
-				current_code_block_char_count = label._get_codeblock_char_count(line, "`")
-				continue
-		elif not within_backtick_block and label._denotes_fenced_code_block(line,"~"):
-			if within_tilde_block:
-				if line.strip_edges().length() >= current_code_block_char_count:
-					code_block = code_block.trim_suffix("\n")
-					_current_paragraph -= 1
-					# code_block += "[/code]"
-					within_tilde_block = false
-					continue
-			else:
-				# code_block += "[code]"
-				within_tilde_block = true
-				current_code_block_char_count = line.strip_edges().length()
-				continue
-		if within_code_block: #ignore any formatting inside code block
-			code_block += label._escape_bbcode(line)
-			continue
-	
-	return code_block
-
-# func _on_markdown_label_gui_input(event:InputEvent):
-# 	print(event)
+		$PanelContainer/v.add_child(node)
+		
