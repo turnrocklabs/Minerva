a7de158da6669c810d90632de4339c0518dd8d54
Banemen <banemen2003@gmail.com>
2024-06-09 00:04:33 +0600

    main
    Added ability to add note from note window

--- src/Scripts/UI/Views/MainScene.gd
+++ src/Scripts/UI/Views/MainScene.gd
@@ -1,5 +1,6 @@
 extends Control
 
+#varibles where weadding out notes Head and descriptionn
 
 func _gui_input(event):
 
@@ -12,3 +13,25 @@ func _gui_input(event):
 		SingletonObject.zoom_ui(-1)
 		
 		accept_event()
+
+#Show the window where we can add note
+func _on_btn_create_note_pressed():
+	#set up windows size
+	var target_size = %VBoxRoot.size - Vector2(200, 200)
+	%CreatNewNote.borderless = false
+	%CreatNewNote.size = target_size
+	%CreatNewNote.popup_centered()
+	
+#Creating new note
+func _on_add_note_pressed():
+	
+	var Head = %CreatNewNote/VBoxContainer/NoteHead
+	var Description = $CreatNewNote/VBoxContainer/NoteDescription
+	
+	if  Head.text == "" or Description.text == "":
+		SingletonObject.ErrorDisplay("Error","You left empty field")
+	else:
+		SingletonObject.NotesTab.add_note(Head.text, Description.text)
+		Head.clear()
+		Description.clear()
+		%CreatNewNote.hide()
