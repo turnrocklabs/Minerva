998e715b9a13ef8e37198024fbf5b071a96f21ff
unknown <cuauhtemoc.ayala@proton.me>
2024-06-22 01:13:06 -0700

    fixed a typo in readme, and fixed chat pane overflow again

--- src/Scripts/Chat.gd
+++ src/Scripts/Chat.gd
@@ -10,9 +10,9 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	# this is for changing the minimum size of the panel and 
 	# changing the text when is being resized
-	if size.x == 500:
+	if size.x < 540:
 		prompt_count_label.text = "Estimated tokens:"
-		custom_minimum_size.x = 450
-	if size.x > 500:
+		custom_minimum_size.x = 480
+	if size.x >= 540:
 		prompt_count_label.text = initial_label_text
 		custom_minimum_size.x = 500
