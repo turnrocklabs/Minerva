cc7246bcc6e41a3d3a0fc6c6d1e12c1626668b92
Banemen <banemen2003@gmail.com>
2024-07-06 22:13:24 +0600

    main
    white board task
    1.whiteboard drawing
    2.erasing
    3.pen support
    4.some visual effects when we drawing and erasing
    5.color pallet to change lines color
    6.save of drawing

--- src/Scripts/UI/Controls/WhiteBoard.gd
+++ src/Scripts/UI/Controls/WhiteBoard.gd
@@ -5,18 +5,20 @@ extends Panel
 @onready var _ColorRect: ColorRect = %White
 @onready var _DrawZone: ColorRect = %DrawZone
 @onready var _ButtonsZone: HBoxContainer = %Buttons
+@onready var _Pen: Sprite2D = %Pen
+@onready var _Eraser: Sprite2D = %Eraser
+@onready var eraser: Button = %eraser
 
 var is_drawing: bool = false
 var BrushSize = 1
 var erase_radius = 1
 var is_eraser_active = false
-var is_erasing = false
 var color = Color(0, 0, 0)
 var mouse_over_color_palette = false
 var mouse_over_buttons_panel = false
 var last_position: Vector2 = Vector2.ZERO
 var last_pressure: float = 0.0
-var is_mouse_down = false 
+var is_mouse_down = false
 
 # Smoothing parameters 
 const SMOOTHING_FACTOR = 0.25 
@@ -24,34 +26,43 @@ const MIN_DISTANCE = 2
 
 func _input(event: InputEvent):
 	var mouse_position = get_local_mouse_position()
-
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			is_mouse_down = event.pressed
 			if is_mouse_down and _is_mouse_in_drawing_zone(mouse_position) and not mouse_over_color_palette and not mouse_over_buttons_panel:
 				is_drawing = true
 				last_position = mouse_position
-				last_pressure = 1.0 
-				create_point(mouse_position, last_pressure) 
+				last_pressure = 1.0
+				if is_eraser_active:
+					erase_line_at(mouse_position)
+				else:
+					create_point(mouse_position, last_pressure)
 			else:
 				is_drawing = false
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 	elif event is InputEventMouseMotion:
-		if is_drawing and event.pressure > 0 and _is_mouse_in_drawing_zone(mouse_position) and not mouse_over_color_palette and not mouse_over_buttons_panel:
-			_DrawZone.visible = true
-			_DrawZone.position = mouse_position
-			smooth_line(last_position, last_pressure, mouse_position, event.pressure)
-			last_pressure = event.pressure 
-			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) 
+		if is_mouse_down and is_drawing and _is_mouse_in_drawing_zone(mouse_position) and not mouse_over_color_palette and not mouse_over_buttons_panel:
+			if is_eraser_active:
+				erase_line_at(mouse_position)
+				_Eraser.visible = true
+				_Eraser.position = mouse_position + Vector2(10, -9)
+			else:
+				_Pen.visible = true
+				_Pen.position = mouse_position + Vector2(10, -9)
+				smooth_line(last_position, last_pressure, mouse_position, event.pressure)
+				last_pressure = event.pressure
+				Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		else:
-			_DrawZone.visible = false
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+			is_drawing = false
+			_Pen.visible = false
+			_Eraser.visible = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func smooth_line(start: Vector2, start_pressure: float, end: Vector2, end_pressure: float):
 	var distance = start.distance_to(end)
 	if distance > MIN_DISTANCE:
-		var smoothed_position = start.lerp(end, SMOOTHING_FACTOR)          
+		var smoothed_position = start.lerp(end, SMOOTHING_FACTOR)
 		var smoothed_pressure = lerp(start_pressure, end_pressure, SMOOTHING_FACTOR)
 		create_line_segment(last_position, last_pressure, smoothed_position, smoothed_pressure)
 		last_position = smoothed_position
@@ -65,17 +76,16 @@ func create_point(position: Vector2, pressure: float):
 	point.add_point(position)
 	point.add_point(position + Vector2(0.1, 0.1))
 	_ColorRect.add_child(point)
-	_DrawZone.size = Vector2(Bsize, Bsize)
 
 func create_line_segment(start: Vector2, start_pressure: float, end: Vector2, end_pressure: float):
 	var line_segment = Line2D.new()
-	var Bsize = BrushSize * (start_pressure + end_pressure) / 2.0 
+	var Bsize = BrushSize * (start_pressure + end_pressure) / 2.0
 	line_segment.default_color = color
 	line_segment.width = Bsize
 	line_segment.add_point(start)
 	line_segment.add_point(end)
 	_ColorRect.add_child(line_segment)
-	_DrawZone.size = Vector2(Bsize, Bsize)
+	_Pen.visible = true
 
 func _on_clear_pressed():
 	for child in _ColorRect.get_children():
@@ -95,7 +105,7 @@ func _on_colors_color_changed(new_color):
 func _on_eraser_pressed():
 	is_eraser_active = !is_eraser_active
 
-func erase_line_at(position):
+func erase_line_at(position: Vector2):
 	for line in _ColorRect.get_children():
 		if line is Line2D:
 			erase_segments_from_line(line, position)
@@ -106,7 +116,7 @@ func erase_segments_from_line(line: Line2D, point: Vector2):
 		var end = line.get_point_position(i + 1)
 		if start.distance_to(point) <= erase_radius or end.distance_to(point) <= erase_radius:
 			line.queue_free()
-			break # Important: Exit the loop after deleting a segment
+			break
 
 func _on_colors_mouse_entered():
 	mouse_over_color_palette = true
@@ -123,7 +133,6 @@ func _on_buttons_mouse_exited():
 	mouse_over_buttons_panel = false
 
 func _is_mouse_in_drawing_zone(mouse_position: Vector2) -> bool:
-	"""Checks if the mouse is within the bounds of the _ColorRect."""
 	var rect_global_pos = _ColorRect.global_position
 	var rect_size = _ColorRect.size
 	return (rect_global_pos.x <= mouse_position.x and 
