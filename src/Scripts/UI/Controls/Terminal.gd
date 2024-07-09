8b5bffe1cd8e6d65e2a7844a966df168ff5edec2
DzIGIBAUER <milosh2255@gmail.com>
2024-07-09 20:32:37 +0200

    Improved terminal, added command history.

--- src/Scripts/UI/Controls/Terminal.gd
+++ src/Scripts/UI/Controls/Terminal.gd
@@ -3,10 +3,20 @@ extends PanelContainer
 
 @onready var command_line_edit: LineEdit = %CommandLineEdit
 @onready var text_edit: TextEdit = %TextEdit
+@onready var cwd_label: Label = %CwdLabel
 
 const cwd_delimiter = "##cwd##"
 
-var cwd = OS.get_data_dir() 
+## History of used commands
+var _history: = PackedStringArray()
+
+var _history_idx = 0
+
+## Holds the current working directory
+var cwd: String:
+	set(value):
+		cwd = value
+		cwd_label.text = "%s>" % cwd
 
 var wrap_command: Callable
 var shell: String
@@ -16,20 +26,22 @@ func _wrap_windows_command(user_input: String) -> PackedStringArray:
 	var full_cmd = [
 		"/V:ON",
 		"/C",
-		"cd /d %s && %s && echo %s!cd!" % [cwd, user_input, cwd_delimiter],
+		"cd /d %s && %s & echo %s!cd!" % [cwd, user_input, cwd_delimiter],
 	]
 
 	return full_cmd
 
 func _wrap_linux_command(user_input: String) -> PackedStringArray:
 	var full_cmd = [
-		'(cd %s && %s && echo "%s$(pwd)")' % [cwd, user_input, cwd_delimiter]
+		'(cd %s && %s & echo "%s$(pwd)")' % [cwd, user_input, cwd_delimiter]
 	]
 
 	return full_cmd
 
 
-func _init():
+func _ready():
+	cwd = OS.get_data_dir()
+
 	match OS.get_name():
 		"Windows":
 			shell = OS.get_environment("COMSPEC")
@@ -67,11 +79,39 @@ func execute_command(input: String) -> void:
 
 	cmd_results = cmd_results.substr(0, cwd_index_start)
 
-	text_edit.text += cmd_results
+	text_edit.text += "%s> %s" % [cwd, cmd_results]
+
+	_history.insert(0, input)
+	_history_idx = -1
 
 
 
 func _on_button_pressed():
-	execute_command(command_line_edit.text)
-	command_line_edit.text = ""
+	if not command_line_edit.text.is_empty():
+		execute_command(command_line_edit.text)
+		command_line_edit.text = ""
+
+
+func _on_command_line_edit_text_submitted(new_text):
+	if not command_line_edit.text.is_empty():
+		execute_command(new_text)
+		command_line_edit.text = ""
+
+
+func _on_command_line_edit_gui_input(event: InputEvent):
+	if event.is_action_pressed("ui_up"):
+		if _history_idx < _history.size() - 1:
+			_history_idx += 1
+			command_line_edit.text = _history[_history_idx]
+			
+			await get_tree().process_frame
+			command_line_edit.caret_column = command_line_edit.text.length()
+
+	elif event.is_action_pressed("ui_down"):
+		if _history_idx > 0:
+			_history_idx -= 1
+			command_line_edit.text = _history[_history_idx]
+			
+			await get_tree().process_frame
+			command_line_edit.caret_column = command_line_edit.text.length()
 
