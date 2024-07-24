85a92ee7503e687e3dbee811996940ea5d921154
DzIGIBAUER <milosh2255@gmail.com>
2024-07-24 08:19:11 +0200

    Added a way to ignore files, added a dialog box for showing messages.

--- src/Scripts/UI/Controls/PackageProjectWindow.gd
+++ src/Scripts/UI/Controls/PackageProjectWindow.gd
@@ -4,7 +4,12 @@ extends PersistentWindow
 @export var destination_label: Label
 @export var package_files_tree: Tree
 @export var file_dialog: FileDialog
+@export var dialog: AcceptDialog
+@export var info_label: Label
 
+@export_category("Icons")
+@export var icon_ignore_true: Texture
+@export var icon_ignore_false: Texture
 @export var directory_icon: Texture
 @export var file_icon: Texture
 
@@ -16,21 +21,62 @@ var save_path: String:
 		destination_label.text = "Save Path: %s" % save_path
 
 
-var com: Dictionary
+## Shows a simple [class AcceptDialog]
+func show_message(title_: String, message: String) -> void:
+	dialog.title = title_
+	dialog.dialog_text = message
+	dialog.popup_centered()
+
 
 func _on_about_to_popup():
 	save_path = "D:/package.minpackage"
-	var common_parents = PackageProject.generate_path_groups(
-		[
-			"D:/shared_proj/test.py",
-			"D:/shared_proj/data/obj.py",
-		]
-	)
+	var common_parents = PackageProject.generate_path_groups(data["Editors"])
 	
 	populate_package_files_tree(common_parents)
 
-	com = common_parents
 
+## Updates the info label with brief of how many files will be packaged.
+func _update_info_label():
+	var root: = package_files_tree.get_root()
+
+	var total: = 0
+	var ignored: = 0
+
+	var items: Array[TreeItem] = [root]
+
+	while items.size() > 0:
+		var item = items.pop_front()
+		if item.get_meta("type", "") == "file":
+			total += 1
+			if item.get_meta("ignored", false):
+				ignored += 1
+
+		items.append_array(item.get_children())
+
+
+	info_label.text = "%s package files (%s ignored)" % [total, ignored]
+	
+
+
+## Sets [param item] ignore state by changing the appearance,[br]
+## item [ignored] meta and button icon.
+func set_item_ignored_state(item: TreeItem, ignored: bool):
+	item.set_meta("ignored", ignored)
+
+	if ignored:
+		item.set_custom_color(0, Color.GRAY)
+		item.clear_custom_bg_color(0)
+		item.set_button(0, 0, icon_ignore_true)
+		item.set_button_tooltip_text(0, 0, "Include Item")
+
+	if not ignored:
+		item.clear_custom_color(0)
+		item.set_custom_bg_color(0, Color(0, 0, 0, 0.5))
+		item.set_button(0, 0, icon_ignore_false)
+		item.set_button_tooltip_text(0, 0, "Ignore Item")
+
+	for child_item in item.get_children():
+		set_item_ignored_state(child_item, ignored)
 
 
 func populate_package_files_tree(common_parents: Dictionary) -> void:
@@ -51,19 +97,30 @@ func populate_package_files_tree(common_parents: Dictionary) -> void:
 					current = current.create_child()
 					current.set_text(0, section)
 					
+					current.add_button(0, icon_ignore_false)
+
 					if section.get_extension().is_empty():
 						current.set_icon(0, directory_icon)
+						
 						current.set_meta("type", "dir")
 					else:
 						current.set_icon(0, file_icon)
 						current.set_meta("type", "file")
 						current.set_meta("original_file", paths_data["original"])
+
 				else:
 					current = section_items.pop_front()
-
+				
+				set_item_ignored_state(current, false)
+	
+	_update_info_label()
 
 ## Recursively loops through tree items and returns all tree items that represent a file
 func _get_file_items(current: TreeItem) -> Array[TreeItem]:
+	# if item is ignored
+	var ignored = current.get_meta("ignored", false)
+	if ignored: return []
+
 	if current.get_meta("type", "") == "file": return [current]
 
 	var items: Array[TreeItem] = []
@@ -109,13 +166,19 @@ func _on_package_button_pressed():
 	var packager: = PackageProject.new()
 
 	packager.package_generation_failed.connect(
-		func(err: int, message: String):
-			print("Error %s: %s" % [error_string(err), message])
-	, CONNECT_ONE_SHOT)
+		func(error: int, message: String):
+			show_message(error_string(error), message)
+	,CONNECT_ONE_SHOT)
 
 	var files_data: = get_final_file_paths()
-	packager.generate_package_file(data, files_data["original_files"], files_data["package_files"], "D:/package.zip")
+	var err: = packager.generate_package_file(data, files_data["original_files"], files_data["package_files"], save_path)
 
+	if not err:
+		show_message("Success", "Successfully saved package at %s" % save_path)
+		dialog.visibility_changed.connect(
+			func(): if not dialog.visible: hide(),
+			CONNECT_ONE_SHOT
+		)
 
 func _on_file_dialog_file_selected(path: String):
 	save_path = path
@@ -124,3 +187,14 @@ func _on_file_dialog_file_selected(path: String):
 func _on_change_destionation_button_pressed():
 	file_dialog.current_path = save_path
 	file_dialog.popup_centered()
+
+
+func _on_files_tree_button_clicked(item: TreeItem, _column: int, _id: int, mouse_button_index: int):
+	if mouse_button_index != MOUSE_BUTTON_LEFT: return
+
+	# current ignored state will be opposite of previously ignored state which is by default false
+	var ignored = not item.get_meta("ignored", false)
+
+	set_item_ignored_state(item, ignored)
+
+	_update_info_label()
