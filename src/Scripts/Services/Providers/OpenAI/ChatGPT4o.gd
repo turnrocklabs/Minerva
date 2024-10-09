f4d565a01c0d5bf4e33388037cbee88c12d2644f
DzIGIBAUER <milosh2255@gmail.com>
2024-10-09 15:50:55 +0200

    Implemented image support for notes for gpt-4o model.

--- src/Scripts/Services/Providers/OpenAI/ChatGPT4o.gd
+++ src/Scripts/Services/Providers/OpenAI/ChatGPT4o.gd
@@ -7,3 +7,64 @@ func _init():
 	model_name = "gpt-4o"
 	short_name = "O4"
 	token_cost = 5.0 / 1000000.0 # https://openai.com/api/pricing/
+
+
+func Format(chat_item: ChatHistoryItem) -> Variant:
+
+	var role: String
+
+	match chat_item.Role:
+		ChatHistoryItem.ChatRole.USER:
+			role = "user"
+		ChatHistoryItem.ChatRole.ASSISTANT:
+			role = "assistant"
+		ChatHistoryItem.ChatRole.SYSTEM:
+			role = "system"
+		ChatHistoryItem.ChatRole.MODEL:
+			role = "assistant"
+
+	# content can be a string, but also an array of dictionaries, to handle different media types
+	# message and each note will be it's own dictionary
+	var content: = [
+		{
+			"type": "text",
+			"text": chat_item.Message
+		},
+	]
+
+	# handle text and image notes
+	for note: Variant in chat_item.InjectedNotes:
+		if note is String:
+			content.append({
+				"type": "text",
+				"text": note
+			})
+		
+		# if we have a image, encode it to base64 and send it as a image_url
+		elif note is Image:
+			content.append({
+				"type": "image_url",
+				"image_url": {
+					"url": "data:image/png;base64,%s" % Marshalls.raw_to_base64(note.save_png_to_buffer())
+				}
+			})
+
+	return {
+		"role": role,
+		"content": content
+	}
+
+# reimplemented to handle image notes properly
+func wrap_memory(item: MemoryItem) -> Variant:
+	# Return either string for text notes or Image for image notes
+
+	if item.MemoryImage:
+		return item.MemoryImage
+	
+	else:
+		var output = "Given this background information:\n\n"
+		output += "### Reference Information ###\n"
+		output += item.Content
+		output += "### End Reference Information ###\n\n"
+		output += "Respond to the user's message: \n\n"
+		return output
