0de71a022e9e83a225c6f9ab4c262dbcffefae0b
DzIGIBAUER <milosh2255@gmail.com>
2024-10-31 17:36:53 +0100

    Added eraser tool and improved layers, added layers selection

--- src/Scripts/UI/Controls/GraphicsEditor/LayerCard.gd
+++ src/Scripts/UI/Controls/GraphicsEditor/LayerCard.gd
@@ -1,7 +1,21 @@
 class_name LayerCard
 extends PanelContainer
 
+signal layer_clicked()
+
 const _scene: = preload("res://Scenes/LayerCard.tscn")
+
+var _active_color: Color = Color.from_string("2d3648", Color.BLACK)
+var _color: Color = Color.from_string("2f2c2c", Color.BLACK)
+
+
+var selected: = false:
+	set(value):
+		selected = value
+		
+		var styleBox: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+		styleBox.set("bg_color", _active_color if selected else _color)
+		add_theme_stylebox_override("panel", styleBox)
 
 var layer: LayerV2:
 	set(value):
@@ -17,6 +31,7 @@ static func create(layer_: LayerV2) -> LayerCard:
 	var lc: LayerCard = _scene.instantiate()
 
 	lc.layer = layer_
+	lc.layer.set_meta("layer_card", lc)
 
 	return lc
 
@@ -33,3 +48,7 @@ func _draw() -> void:
 
 func _on_visibility_check_button_toggled(toggled_on: bool) -> void:
 	layer.visible = toggled_on
+
+
+func _on_layer_card_pressed() -> void:
+	layer_clicked.emit()
