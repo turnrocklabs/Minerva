2798178007e48fda9739584282c2c3f7a56b374b
Banemen <94903514+kkaktusenok@users.noreply.github.com>
2024-11-17 18:08:10 +0500

    DragAndDrop
    ready

--- src/Scripts/DropScript.gd
+++ src/Scripts/DropScript.gd
@@ -1,9 +1,7 @@
 extends ColorRect
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	print("Can drop: ", data)
 	return true
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	prints("Drop:", at_position, data)
 	SingletonObject.NotesTab.add_note(data, data)
