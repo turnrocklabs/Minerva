114f8803279194d7a4a4cf249225f91ca62e8ffd
Banemen <94903514+kkaktusenok@users.noreply.github.com>
2024-11-24 00:12:45 +0500

    Small changes
    we changed tab name for dragged notes

--- src/Scripts/DropScript.gd
+++ src/Scripts/DropScript.gd
@@ -4,4 +4,4 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 	return true
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	SingletonObject.NotesTab.add_note(data, data)
+	SingletonObject.NotesTab.add_note("Drag Note", data)
