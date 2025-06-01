d6c628be3c88b6391e7db27e27cf160d51d41a1e
Banemen <94903514+kkaktusenok@users.noreply.github.com>
2025-06-02 03:45:38 +0500

    fixed Drawer moving Notes
    fixed bug that not allowed us move notes between Drawer threads

--- src/Scripts/UI/Views/DrawerTabs.gd
+++ src/Scripts/UI/Views/DrawerTabs.gd
@@ -1,21 +1,12 @@
 class_name DrawerTabs
 extends TabContainer
 
-@onready var tcThreads = %tcThreadsDrawer
-# just use current_tab
-# var ActiveThreadIndex: int:
-@onready var buffer_control_notes: Control = %BufferControlNotes
 
 var _drag_active := true
 # var _hovered_tab := -1
 # var _hover_timer
 
-var isDrawer = false
 
-# This flag will be set to true when we need to update the UI
-var _needs_update := false
-#var _can_drop:bool = false
-## return a single large string of all active memories
 func To_Prompt(provider: BaseProvider) -> Array[Variant]:
 	var output: Array[Variant] = []
 	
@@ -70,59 +61,8 @@ func disable_notes_in_tab():
 		if item.Enabled:
 			item.Enabled = false
 
-#endregion Methods for toggling notes
-
-func open_threads_popup(tab_name: String = "", tab = null):
-	
-	var update = tab != null
-	
-	# set metadata so we can determine should we create new or update existing and which tab, when we click the button in the popup
-	if update:
-		SingletonObject.associated_notes_tab.emit(tab_name, get_child(tab))
-	else: 
-		SingletonObject.pop_up_new_drawer_tab.emit()
-
-
-func _on_new_pressed():
-	open_threads_popup()
-
-
-func _on_btn_create_thread_pressed(isDrawerNote:bool,tab_name: String, tab_ref: Control = null):
-	#added a check for the tab name, if no name gives a default name
-	if isDrawerNote:
-		if tab_name == "":
-			tab_name = "notes " + str(%tcThreadsDrawer.get_tab_count() + 1)
-	
-		if tab_ref:
-			tab_ref.get_meta("thread").ThreadName = tab_name
-			render_threads()
-		else:
-			create_new_notes_tab(tab_name)
-	
-		if get_tab_count() > 0:
-			buffer_control_notes.hide()
-
 ## add indexing system here
 var new_tab: bool = false
-func create_new_notes_tab(tab_name: String = "notes 1"):
-	var thread = MemoryThread.new()
-	thread.ThreadName = tab_name_to_use(tab_name)
-	var thread_memories: Array[MemoryItem] = []
-	thread.MemoryItemList = thread_memories
-	new_tab = true
-	SingletonObject.DrawerThreadList.append(thread)
-	render_thread(thread)
-
-
-func tab_name_to_use(proposed_name: String) -> String:
-	var collisions = 0
-	for i in range(tcThreads.get_tab_count()):
-		if tcThreads.get_tab_title(i).split(" ")[0] == proposed_name:
-			collisions+=1
-	if collisions == 0:
-		return proposed_name
-	else:
-		return proposed_name + "(" + str(tcThreads.get_tab_count() + 1) + ")"
 
 
 func clear_all_tabs():
@@ -133,131 +73,6 @@ func clear_all_tabs():
 	pass
 	
 
-
-#region Add notes methods
-
-func add_note(user_title:String, user_content: String,is_completed:bool = true, _source: String = "") -> MemoryItem:
-	# get the active thread.
-	if (SingletonObject.DrawerThreadList == null) or current_tab < 0:
-		#SingletonObject.ErrorDisplay("Missing Thread", "Please create a new notes tab first, then try again.")
-		#return
-		create_new_notes_tab()
-	
-	var active_thread : MemoryThread = SingletonObject.DrawerThreadList[current_tab]
-	
-	# Create a memory item.
-	var new_memory: MemoryItem = MemoryItem.new(active_thread.ThreadId)
-	new_memory.UUID = SingletonObject.generate_UUID()
-	new_memory.Enabled = false
-	new_memory.Type = SingletonObject.note_type.TEXT
-	new_memory.ContentType = "text"
-	new_memory.Title = user_title
-	new_memory.Content = user_content           
-	new_memory.Visible = true
-	new_memory.isCompleted = is_completed
-	
-	# append the new memory item to the active thread memory list
-	active_thread.MemoryItemList.append(new_memory)
-	#buffer_control_notes.hide()
-	render_threads()
-
-	return new_memory
-
-
-
-
-func add_audio_note(note_title: String, note_audio: AudioStreamWAV) -> MemoryItem:
-	if (SingletonObject.DrawerThreadList == null) or current_tab < 1:
-		#SingletonObject.ErrorDisplay("Missing Thread", "Please create a new notes tab first, then try again.")
-		#return
-		await create_new_notes_tab()
-	
-	var active_thread : MemoryThread = SingletonObject.DrawerThreadList[self.current_tab]
-	
-	# Create a memory item.
-	var new_memory: MemoryItem = MemoryItem.new(active_thread.ThreadId)
-	new_memory.UUID = SingletonObject.generate_UUID()
-	new_memory.Enabled = false
-	new_memory.Type = SingletonObject.note_type.AUDIO
-	new_memory.ContentType = "audio"
-	new_memory.Title = note_title
-	new_memory.Audio = note_audio
-	new_memory.Visible = true
-	
-	# append the new memory item to the active thread memory list
-	active_thread.MemoryItemList.append(new_memory)
-	#buffer_control_notes.hide()
-	render_threads()
-	return new_memory
-
-
-func add_image_note(note_title: String, note_image: Image, imageCaption: String = "") -> MemoryItem:
-	if SingletonObject.DrawerThreadList.is_empty():
-		create_new_notes_tab()
-	
-	var active_thread : MemoryThread = SingletonObject.DrawerThreadList[self.current_tab]
-	
-	# Create a memory item.
-	var new_memory: MemoryItem = MemoryItem.new(active_thread.ThreadId)
-	new_memory.UUID = SingletonObject.generate_UUID()
-	new_memory.Enabled = false
-	new_memory.Type = SingletonObject.note_type.IMAGE
-	new_memory.ContentType = "image"
-	new_memory.Title = note_title
-	new_memory.MemoryImage = note_image
-	new_memory.ImageCaption = imageCaption
-	new_memory.Visible = true
-	
-	# append the new memory item to the active thread memory list
-	active_thread.MemoryItemList.append(new_memory)
-	#buffer_control_notes.hide()
-	render_threads()
-	return new_memory
-	
-	
-## Creates a note without adding it to any thread.
-func create_note(title: String, type: SingletonObject.note_type = SingletonObject.note_type.TEXT) -> MemoryItem:
-	var new_memory: MemoryItem = MemoryItem.new()
-	new_memory.UUID = SingletonObject.generate_UUID()
-	new_memory.Enabled = false
-	new_memory.Type = type
-	new_memory.Title = title
-	new_memory.Visible = true
-
-	return new_memory
-
-#endregion Add notes methods
-
-#region Update Notes methods
-
-func update_note(note_UUID: String, new_data: Variant) -> MemoryItem:
-	# we check for the note in the current thread so that we dont loop over all the tabs
-	if note_UUID == "" or new_data == null:
-		return null
-	var current_thread: MemoryThread = SingletonObject.DrawerThreadList[current_tab]
-	if current_thread:
-		for item in current_thread.MemoryItemList:
-			if item.UUID == note_UUID:
-				print("found the item in the current thread")
-				return update_note_handler(item, new_data)
-	
-	# if the note is not found in the current thread we loop over all the tabs
-	var item: MemoryItem = get_memory_item(note_UUID)
-	if item:
-		return update_note_handler(item, new_data)
-		
-	printerr("memorty item not found :c")
-	return null
-
-
-func get_memory_item(memory_item_UUID: String) -> MemoryItem:
-	for thread: MemoryThread in SingletonObject.DrawerThreadList:
-		for item: MemoryItem in thread.MemoryItemList:
-			if item.UUID == memory_item_UUID:
-				return item
-	return null
-
-
 func update_note_handler(item: MemoryItem, new_data: Variant) -> MemoryItem:
 	if item.Type == SingletonObject.note_type.TEXT:
 		item.Content = new_data as String
@@ -267,17 +82,6 @@ func update_note_handler(item: MemoryItem, new_data: Variant) -> MemoryItem:
 		item.Audio = new_data as AudioStreamWAV
 	render_threads()
 	return item
-
-#endregion Update Notes methods
-
-## Will delete the memory_item from the memory list
-func delete_note(memory_item: MemoryItem):
-	var active_thread : MemoryThread = SingletonObject.DrawerThreadList[self.current_tab]
-
-	var idx = active_thread.MemoryItemList.find(memory_item)
-	if idx == -1: return
-	
-	active_thread.MemoryItemList.remove_at(idx)
 
 
 func render_threads():
@@ -331,219 +135,8 @@ func render_thread(thread_item: MemoryThread):
 
 
 
-
-func _on_close_tab(tab: int, container: TabContainer):
-	var control = container.get_tab_control(tab)
-	
-	# This is the thread index in the list, not it's id
-	var thread_idx = SingletonObject.DrawerThreadList.find(control.get_meta("thread"))
-	if thread_idx != -1:
-		# Remove the thread from the list
-		SingletonObject.DrawerThreadList.remove_at(thread_idx)
-
-		# this will crash the program by freeing the `control` object
-		# Update the UI with the remaining threads
-		#render_threads()
-
-		# Store deleted tab for potential undo
-		SingletonObject.undo.store_deleted_tab_right(tab, control, "right")
-	
-	# Remove the tab control from the TabContainer
-	container.remove_child(control) 
-	#control.queue_redraw()
-	
-	if get_tab_count() < 1:
-		buffer_control_notes.show()
-	
-	
-func restore_deleted_tab(tab_name: String):
-	if tab_name in SingletonObject.undo.deleted_tabs:
-		var data = SingletonObject.undo.deleted_tabs[tab_name]
-		
-		var control = data["control"]
-		data["timer"].stop()
-		# Get the MemoryThread associated with the tab.
-		var thread: MemoryThread = control.get_meta("thread")
-
-		# Re-add the MemoryThread to the ThreadList if it's not already present.
-		if SingletonObject.DrawerThreadList.find(thread) == -1:
-			SingletonObject.DrawerThreadList.append(thread)
-
-		# Call render_thread to re-create the tab UI.
-		render_thread(thread)
-
-		# Remove the data from the deleted_tabs dictionary.
-		SingletonObject.undo.deleted_tabs.erase(tab_name)
-	
-func _memory_thread_find(thread_id: String) -> MemoryThread:
-	return SingletonObject.DrawerThreadList.filter(
-		func(t: MemoryThread):
-			return t.ThreadId == thread_id
-	).pop_front()
-
-
-
-## Function:
-# attach_file creates a memory item/note from a file.  It can detect file type
-func attach_file(the_file: String):
-	# Check if the file exists
-	var file = FileAccess.open(the_file, FileAccess.READ)
-	if file == null:
-		SingletonObject.ErrorDisplay("File Error", "The file could not be opened.")
-		return
-		
-	var file_ext = the_file.get_extension().to_lower()
-	
-	# Determine the file type
-	@warning_ignore("unused_variable")
-	var file_type = ""
-	var content = ""
-	var content_type = ""
-	var type
-	var title = the_file.get_file()
-	
-	# Get the active thread
-	if (SingletonObject.DrawerThreadList == null) or current_tab < 0:
-		create_new_notes_tab()
-	var active_thread: MemoryThread = SingletonObject.DrawerThreadList[self.current_tab]
-	
-	var new_memory: MemoryItem = MemoryItem.new(active_thread.ThreadId)
-	new_memory.File = the_file # associate the file with the new memory item
-	
-	if _is_text_file(the_file):
-		type = SingletonObject.note_type.TEXT
-		content_type = "text/plain"
-		content = file.get_as_text()
-	elif file_ext in SingletonObject.supported_image_formats:
-		file_type = "image"
-		type = SingletonObject.note_type.IMAGE
-		var file_data = file.get_buffer(file.get_length())
-		var image: Image = Image.new()
-		var err: Error = OK
-		match file_ext:
-			"svg":
-				err = image.load_svg_from_buffer(file_data)
-			"jpeg", "jpg":
-				err = image.load_jpg_from_buffer(file_data)
-			"png":
-				err = image.load_png_from_buffer(file_data)
-			"bmp":
-				err = image.load_bmp_from_buffer(file_data)
-			"webp":
-				err = image.load_webp_from_buffer(file_data)
-			"tga":
-				err = image.load_tga_from_buffer(file_data)
-		if err == OK:
-			new_memory.MemoryImage = image
-			content = Marshalls.raw_to_base64(file_data)
-		else:
-			printerr("Error loading image file %s" % file)
-			SingletonObject.ErrorDisplay("Error loading image", "An error occurred while trying to load the image file %s" % file)
-		content_type = "image/%s" % file_ext
-	elif file_ext in SingletonObject.supported_video_formats:
-		file_type = "video"
-		type = SingletonObject.note_type.VIDEO
-		content = the_file
-		content_type = "video/%s" % file_ext
-	elif file_ext in SingletonObject.supported_audio_formats:
-		file_type = "audio"
-		type = SingletonObject.note_type.AUDIO
-		var buffer = file.get_buffer(file.get_length())
-		match file_ext:
-			"mp3":
-				var mp3AudioStream = AudioStreamMP3.new()
-				mp3AudioStream.data = buffer
-				new_memory.Audio = mp3AudioStream
-			"wav":
-				var wavAudioStream = AudioStreamWAV.load_from_buffer(buffer)
-				new_memory.Audio = wavAudioStream
-			"ogg":
-				var oggAudioStream = AudioStreamOggVorbis.load_from_file(the_file)
-				new_memory.Audio = oggAudioStream
-		content = Marshalls.raw_to_base64(buffer)
-		content_type = "audio/%s" % file_ext
-	elif _is_binary_file(the_file):
-		# Generic binary file handling
-		type = SingletonObject.note_type.BINARY
-		content_type = "application/octet-stream"
-		content = Marshalls.raw_to_base64(file.get_buffer(file.get_length()))
-	else:
-		# Fallback to text handling
-		type = SingletonObject.note_type.TEXT
-		content_type = "text/plain"
-		content = file.get_as_text()
-
-	# Create a new memory item
-	new_memory.Enabled = true
-	new_memory.Title = title
-	new_memory.Content = content
-	new_memory.ContentType = content_type
-	new_memory.Type = type
-	new_memory.Visible = true
-	
-	# Append the new memory item to the active thread memory list
-	active_thread.MemoryItemList.append(new_memory)
-	render_threads()
-
-	file.close()
-# Helper function to check if a file is binary (opposite of text file)
-func _is_binary_file(file_path: String) -> bool:
-	var file = FileAccess.open(file_path, FileAccess.READ)
-	if file == null:
-		return false  # Can't open file, assume it's not binary
-	
-	# Read first 1024 bytes to check
-	var buffer: = file.get_buffer(1024)
-	if buffer.is_empty(): 
-		return false  # Empty file is not binary
-	
-	for byte in buffer:
-		# Binary files typically contain control characters (0-8, 14-31) 
-		# except for common whitespace characters (\t, \n, \r)
-		if byte < 9 or (byte > 13 and byte < 32):
-			file.close()
-			return true
-	
-	file.close()
-	return false
-	
-# helper func to check if the file is text
-func _is_text_file(file_path: String) -> bool:
-	var file = FileAccess.open(file_path, FileAccess.READ)
-	if file == null:
-		return false
-
-	var is_text: bool = false
-	var buffer: = file.get_buffer(1024)  # Read the first 1024 bytes
-	if buffer.is_empty(): return true
-	for byte in buffer:
-		# Check for non-text characters (control characters outside of \t, \n, \r)
-		if byte < 9 or (byte > 13 and byte < 32):
-			is_text = false
-			break
-		else:
-			is_text = true
-
-	file.close()
-	return is_text
-	
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	%tcThreadsDrawer.get_tab_bar().tab_close_display_policy = TabBar.CLOSE_BUTTON_SHOW_ALWAYS
-	%tcThreadsDrawer.get_tab_bar().tab_close_pressed.connect(_on_close_tab.bind(%tcThreadsDrawer))
-		
-	# tab bar need mouse_filter set to pass to allow the tab container to catch drag event and call _can_drop_data
 	get_tab_bar().mouse_filter = MOUSE_FILTER_PASS
-
-	# Connect signals for changes in your data
-	# SingletonObject.connect("ThreadListChanged", self._on_thread_list_changed) 
-
-	SingletonObject.DrawerThreadList = []
-	SingletonObject.DrawerTab = self
-	SingletonObject.AttachNoteFile.connect(self.attach_file)
-	SingletonObject.create_notes_tab.connect(_on_btn_create_thread_pressed)
-	render_threads()
 
 
 # if we are dragging a note above a tab, we can drop it there
@@ -567,37 +160,6 @@ func _drop_data(at_position: Vector2, data):
 	vbox_memory_list._drop_data(at_position, data)
 	current_tab = tab_idx
 	
-func _notification(what):
-	match what:
-		NOTIFICATION_DRAG_BEGIN: _drag_active = true
-		NOTIFICATION_DRAG_END: _drag_active = false
-
-#region Tab signal methods
-
-var clicked: = -1 # this is used to tack double click to change the tab nama
-var temp_current_tab: = -1 # this is used to track the clicked tab when rearranged
-
-
-func _on_active_tab_rearranged(idx_to: int) -> void:
-	var temp_threadList = SingletonObject.DrawerThreadList
-	var chat_history_to_move: MemoryThread = SingletonObject.DrawerThreadList[temp_current_tab]
-	temp_threadList.pop_at(temp_current_tab)
-	SingletonObject.DrawerThreadList.insert(idx_to, chat_history_to_move)
-	temp_current_tab = current_tab
-
-#endregion Tab signal methods
-
-
-# This function is called when the ThreadList is modified
-func _on_thread_list_changed():
-	_needs_update = true
-
-# This function is called every frame
-func _process(_delta):
-	if _needs_update:
-		render_threads()
-		_needs_update = false
-
 
 # FIXME: This will interfere with render threads on project load
 # one note tab is loaded and then since child is added this function is called
