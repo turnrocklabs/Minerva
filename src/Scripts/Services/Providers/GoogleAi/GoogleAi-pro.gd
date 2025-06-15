8fc1425a3b99ed5045983587a2e92366ae3d723c
Imran Peerbhai <ipeerbhai@aol.com>
2025-06-14 17:23:41 -0700

    Ramora -- update to 06-06 Gemini Pro Preview

--- src/Scripts/Services/Providers/GoogleAi/GoogleAi-pro.gd
+++ src/Scripts/Services/Providers/GoogleAi/GoogleAi-pro.gd
@@ -9,7 +9,7 @@ func _init():
 	BASE_URL = "https://generativelanguage.googleapis.com/v1beta/models"
 	PROVIDER = SingletonObject.API_PROVIDER.GOOGLE
 
-	model_name = "gemini-2.5-pro-preview-05-06"
+	model_name = "gemini-2.5-pro-preview-06-05"
 	short_name = "GP"
 	token_cost = 1.25 / 1_000_000 # https://claude101.com/claude-3-5-sonnet/
 
