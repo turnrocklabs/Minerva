646f40935aa8d6256bc345e5a4b182c6cfe26643
Milos <milosh2255@gmail.com>
2025-11-01 19:55:28 +0100

    Bux fixes(core connection, tabs, whisper api) and remove duplicate graphics editor classes

--- src/GraphicsEditor/addons/gd_uuid/base_random.gd
+++
@@ -1,7 +0,0 @@
-class_name BaseRandom
-
-
-func bytes(size: int) -> PackedByteArray:
-	var result = PackedByteArray()
-	result.resize(size)
-	return result
--- src/GraphicsEditor/addons/gd_uuid/crypto_random.gd
+++
@@ -1,8 +0,0 @@
-class_name CryptoRandom
-extends BaseRandom
-
-static var _crypto = Crypto.new()
-
-
-func bytes(size: int) -> PackedByteArray:
-	return _crypto.generate_random_bytes(size)
